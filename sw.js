99725f7db7777094381f9aeb89e5ccdb36cdbd10
iurli070 <d.iurlaro@hotmail.it>
2026-09-25 22:25:36 +0200

    Refactor service worker for caching and fetching

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-const CACHE='oggi-v2';const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = "oggi-planner-v4";
+ 
+self.addEventListener("install", (event) => {
+event.waitUntil(
+caches.open(CACHE).then((cache) => {
+return cache.addAll([
+"./",
+"./index.html",
+"./manifest.webmanifest"
+]);
+})
+);
+});
+ 
+self.addEventListener("fetch", (event) => {
+event.respondWith(
+caches.match(event.request).then((response) => {
+return response || fetch(event.request);
+})
+);
+});
+``
